d35ca954c8301bc97c6228c8e1c8f2f86be82c63
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2023-12-18 12:31:49 -0300

    javascript

---
+++ javascript/incluindojs/script.js
@@ -0,0 +1,7 @@
+alert('Olá, seja bem vindo ao curso!')
+
+/* 
+    Selecionar um elemento no DOM
+    Atualizar o valor desse elemento com uma String
+*/
+document.getElementById('nome').value = 'Oi'
